4ea5562178630db51f0fc27096674f9fb778cd3a
tsupplis <tsupplis@users.noreply.github.com>
2026-09-10 02:36:00 +0100

    improve info output

--- cmdinfo.c
+++ cmdinfo.c
@@ -47,6 +47,30 @@ typedef unsigned int size_t;
 #define basename(a) (a)
 #endif
 
+/*
+The .CMD file is the general executable format used by CP/M-86 and its derivatives. As well as standalone executable files, it is also used for other files containing code under CP/M-86, such as:
+
+System files — for example, the DOS Plus boot file DOSPLUS.SYS is in this format.
+GSX-86 drivers for both CP/M and DOS.
+The filetype for a standalone program is usually .CMD. Under "Speedstart CP/M", a cut-down version of CP/M-86 intended for embedded use, it is instead .STM, and the first group is obfuscated (see below).
+
+A .CMD file begins with a 128-byte header. The first byte will always be 1-9, of which 1 is by far the most common. If a file has a .CMD extension and starts with any other byte, it is most likely a Windows NT script file rather than a CMD file in this format.
+*/
+
+/*
+	DB	type	        ;1-4 for Code/Data/Extra/Stack
+				        ;5-8 for Aux1/Aux2/Aux3/Aux4
+				        ;9 for "pure" code (that can be shared 
+				        ;between processes).
+	DW	length		    ;}
+	DW	base		    ;} In paragraphs
+	DW	minimum size	;}
+	DW	maximum size	;}
+;
+; The "base" of a group is 0 if a segment is relocatable. Normally it is
+; nonzero only in the operating system file (DOSPLUS.SYS / PCPM.SYS / CPM.SYS).
+;
+*/
 struct PACK _header_t {
     uint8_t form;
     uint16_t length;
@@ -57,12 +81,66 @@ struct PACK _header_t {
 
 typedef struct _header_t header_t;
 
+/*
+Offset 0: 8 group descriptors [72 bytes]. Each descriptor is:
+
+;
+; The following fields are not supported in CP/M-86 v1.x, but are in the 4.x
+; kernel:
+;
+Offset 7Bh: DW RSX index record, 0 if no RSX index
+Offset 7Dh: DW 1st record with fixups, 0 if no fixups
+Offset 7Fh: DB flags
+	Bit 4 set: This file is an RSX, not a CMD file
+	Bit 5 set: Attempt to allocate the 8087 to this program, but only if
+                  there is an 8087 present.
+	Bit 6 set: Attempt to allocate the 8087 to this program. Even if 
+                  there isn't one in the computer, allocate an imaginary
+                  resource for it.
+	Bit 7 set: Do segment fixups.
+*/
 struct PACK _header_block_t {
     header_t header[8];
-    uint8_t padding[56];
+    uint8_t padding[51];
+    uint16_t rsx_index_rec;
+    uint16_t fixup_first_rec;
+    uint8_t flags;
 };
 
 typedef struct _header_block_t header_block_t;
+
+/*
+	DB	xyH	;x=Source group, 1-8. y=Dest group, 1-8.
+	DW	offset to add to source segment register
+	DB	offset to add to source offset, 0-15.
+*/
+struct PACK _fixup_record_t {
+    uint8_t source_dest_group;
+    uint16_t source_segment_offset;
+    uint8_t source_offset_offset;
+};
+
+typedef struct _fixup_record_t fixup_record_t;
+
+/*
+	DW	xxxx	    ;Offset of RSX from the end of the CMD header,
+				    ;in the file, in 128-byte records minus 1.
+				    ;0000h  indicates the RSX is linked 
+				    ;       dynamically and must be loaded from 
+				    ;       disc when the file is loaded.
+				    ;0FFFFh indicates end of list
+	DB	'RSXNAME '	;Name of the .RSX. If the RSX is linked 
+				    ;dynamically, this is its filename on disc.
+	DW	0,0,0		;Unused
+*/
+struct PACK _rsx_record_t {
+    uint16_t rsx_offset;
+    uint8_t rsx_name[8];
+    uint16_t unused[3];
+};
+
+typedef struct _rsx_record_t rsx_record_t;
+
 
 char *g_type[16] = {"NULL",      "CODE",        "DATA",      "EXTRA",
                     "STACK",     "AUX #1",      "AUX #2",    "AUX #3",
@@ -156,15 +234,19 @@ display_header(out, name, index, header, offset, fin)
         return;
     }
     type = header->form & 0xF;
-    fprintf(out, "INF: HDR(%d)",index);
-    fprintf(out, "TYP(%02d", header->form);
+    fprintf(out, "INF:     HDR(%d)",index);
+    fprintf(out, " TYPE(%02d", header->form);
     fprintf(out, ",%s)", g_type[header->form]);
-    fprintf(out, "BAS(%04xh)", header->base);
+    if(header->base) {
+    fprintf(out, " BASE(FIXED,%04xh)", header->base);
+    } else {
+        fprintf(out, " BASE(RELOCATABLE)");
+    }
     if (header->min)
-        fprintf(out, "MN(%.1fk=%lu)", header->min / 64.0, (unsigned long)header->min * 16);
+        fprintf(out, " MIN(%.1fk=%lu)", header->min / 64.0, (unsigned long)header->min * 16);
     if (header->max)
-        fprintf(out, "MX(%.1fk=%lu)", header->max / 64.0, (unsigned long)header->max * 16);
-    fprintf(out, "LEN(%lu)", (unsigned long)header->length*16);
+        fprintf(out, " MAX(%.1fk=%lu)", header->max / 64.0, (unsigned long)header->max * 16);
+    fprintf(out, " LEN(%lu)", (unsigned long)header->length*16);
     fprintf(out, "\n");
     if (type == 1 || type == 2 || type == 3) {
         dump(out, index, (char)tolower(g_type[type][0]),
@@ -188,6 +270,44 @@ display_header_block(out, name,  block, fin)
     int i;
     long offset = 128;
 
+    fprintf(out,"INF: FILE(%s)\n",name);
+    fprintf(out,"INF:   BLOCK");
+    if(block->rsx_index_rec) {
+        fprintf(out, " RSX(%u)", block->rsx_index_rec);
+    }
+    if(block->fixup_first_rec){
+        fprintf(out, " FIXUP(%u)", block->fixup_first_rec);
+    }
+    if(block->flags&0x08) {
+        fprintf(out, " FLAGS(RSX,");
+    } else {
+        fprintf(out, " FLAGS(CMD,");
+    }
+    if(block->flags&0x10) {
+        fprintf(out, "P8087,");
+    } else {
+        fprintf(out, "N8087,");
+    }
+    if(block->flags&0x20) {
+        fprintf(out, "F8087,");
+    } else {
+        fprintf(out, "N8087,");
+    }
+    if(block->flags&0x40) {
+        fprintf(out, "FIXUP");
+        offset+=block->fixup_first_rec*4;
+    } else {
+        fprintf(out, "NFIXUP");
+    }
+    fprintf(out,")\n");
+    /* dump fixups if any */
+   if(block->flags&0x40) {
+        offset+=block->fixup_first_rec*4;
+    } 
+    /* dump rsx if any */
+    if(block->rsx_index_rec) {
+        offset+=block->rsx_index_rec*16;
+    }
     for (i = 0; i < 8; i++) {
         display_header(out, name, i, block->header + i, &offset, fin);
     }
